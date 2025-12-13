a4ec042bdee7d40ccb986e84ba9bfe7435d3ac24
Thomas Beirne <thomas@ediphi.com>
2025-12-12 20:19:14 -0800

    Day 4, part 1

--- src/04/index.test.ts
+++ src/04/index.test.ts
@@ -1,14 +1,92 @@
 import { describe, it, expect } from "vitest";
-import { part1, part2 } from "./index";
+import {
+  countNeighboringRolls,
+  getNeighborCoords,
+  parseDiagram,
+  part1,
+  part2,
+} from "./index";
 
-const testLines = ``.split("\n");
+const testLines = `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`.split("\n");
 
 describe("Day 04", () => {
-  describe("Part 1", () => {
+  describe("parseDiagram", () => {
     it("works", () => {
-      expect(part1(testLines)).toBe(0);
+      const { width, height, rollsCoords } = parseDiagram(testLines);
+      expect(width).toBe(10);
+      expect(height).toBe(10);
+      expect(rollsCoords.has("0,1")).toBe(true);
+      expect(rollsCoords.has("2,0")).toBe(true);
+      expect(rollsCoords.has("0,0")).toBe(false);
+      expect(rollsCoords.has("9,9")).toBe(false);
     });
   });
+
+  describe("getNeighborCoords", () => {
+    it("works in the top left corner", () => {
+      const neighborCoords = getNeighborCoords(
+        { x: 0, y: 0 },
+        { width: 5, height: 5 }
+      );
+
+      expect(neighborCoords.length).toBe(3);
+      expect(neighborCoords).toContainEqual({ x: 0, y: 1 });
+      expect(neighborCoords).toContainEqual({ x: 1, y: 1 });
+      expect(neighborCoords).toContainEqual({ x: 1, y: 0 });
+    });
+
+    it("works in the middle", () => {
+      const neighborCoords = getNeighborCoords(
+        { x: 1, y: 1 },
+        { width: 3, height: 3 }
+      );
+
+      expect(neighborCoords.length).toBe(8);
+      expect(neighborCoords).toContainEqual({ x: 0, y: 0 });
+      expect(neighborCoords).toContainEqual({ x: 0, y: 1 });
+      expect(neighborCoords).toContainEqual({ x: 0, y: 2 });
+      expect(neighborCoords).toContainEqual({ x: 1, y: 0 });
+      expect(neighborCoords).toContainEqual({ x: 1, y: 2 });
+      expect(neighborCoords).toContainEqual({ x: 2, y: 0 });
+      expect(neighborCoords).toContainEqual({ x: 2, y: 1 });
+      expect(neighborCoords).toContainEqual({ x: 2, y: 2 });
+    });
+
+    it("works in the bottom right corner", () => {
+      const neighborCoords = getNeighborCoords(
+        { x: 4, y: 4 },
+        { width: 5, height: 5 }
+      );
+
+      expect(neighborCoords.length).toBe(3);
+      expect(neighborCoords).toContainEqual({ x: 3, y: 4 });
+      expect(neighborCoords).toContainEqual({ x: 3, y: 3 });
+      expect(neighborCoords).toContainEqual({ x: 4, y: 3 });
+    });
+  });
+
+  describe("countNeighboringRolls", () => {
+    it("works", () => {
+      const diagram = parseDiagram(testLines);
+      expect(countNeighboringRolls({ x: 0, y: 0 }, diagram)).toBe(2);
+    });
+  });
+
+  describe("Part 1", () => {
+    it("works", () => {
+      expect(part1(testLines)).toBe(13);
+    });
+  });
+
   describe("Part 2", () => {
     it("works", () => {
       expect(part2(testLines)).toBe(0);
--- src/04/index.ts
+++ src/04/index.ts
@@ -1,8 +1,74 @@
+import { parse } from "path";
 import { readLines } from "../../util/readLines";
+
+type CoordStr = `${number},${number}`;
+type Diagram = {
+  width: number;
+  height: number;
+  rollsCoords: Set<CoordStr>;
+};
+export function parseDiagram(lines: string[]): Diagram {
+  const width = lines[0].length;
+  const height = lines.length;
+  const rollsCoords = new Set<CoordStr>();
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      if (lines[y][x] === "@") {
+        rollsCoords.add(`${x},${y}`);
+      }
+    }
+  }
+
+  return { width, height, rollsCoords };
+}
+
+type Coord = { x: number; y: number };
+export function parseCoordStr(cs: CoordStr): Coord {
+  const [x, y] = cs.split(",").map((s) => parseInt(s, 10));
+  return { x, y };
+}
+
+export function addCoordStrs(cs1: CoordStr, cs2: CoordStr): CoordStr {
+  const { x, y } = addCoords(parseCoordStr(cs1), parseCoordStr(cs2));
+  return `${x},${y}`;
+}
+
+export function areCoordsEqual(c1: Coord, c2: Coord): boolean {
+  return c1.x === c2.x && c1.y === c2.y;
+}
+
+export function addCoords(c1: Coord, c2: Coord) {
+  return { x: c1.x + c2.x, y: c1.y + c2.y };
+}
+
+export function getNeighborCoords(
+  coord: Coord,
+  { width, height }: { width: number; height: number }
+): Coord[] {
+  return [-1, 0, 1]
+    .flatMap((deltaX) =>
+      [-1, 0, 1].map((deltaY) => addCoords(coord, { x: deltaX, y: deltaY }))
+    )
+    .filter((neighbor) => !areCoordsEqual(coord, neighbor))
+    .filter((neighbor) => neighbor.x >= 0 && neighbor.y >= 0)
+    .filter((neighbor) => neighbor.x < width && neighbor.y < width);
+}
+
+export function countNeighboringRolls(coord: Coord, diagram: Diagram) {
+  return getNeighborCoords(coord, diagram).filter((c) =>
+    diagram.rollsCoords.has(`${c.x},${c.y}`)
+  ).length;
+}
 
 // Part 1
 export function part1(lines: string[]) {
-  return 0;
+  const diagram = parseDiagram(lines);
+  return [...diagram.rollsCoords.values()].reduce((acc, cs) => {
+    return countNeighboringRolls(parseCoordStr(cs), diagram) < 4
+      ? acc + 1
+      : acc;
+  }, 0);
 }
 
 // Part 2
