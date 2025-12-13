295a90c716c66801f0ec8fd66183d0dcdefd9c2c
Thomas Beirne <thomas@ediphi.com>
2025-12-12 20:39:27 -0800

    Day 5, part 1

--- src/05/index.test.ts
+++ src/05/index.test.ts
@@ -1,12 +1,39 @@
 import { describe, it, expect } from "vitest";
-import { part1, part2 } from "./index";
+import { parseDb, part1, part2 } from "./index";
 
-const testLines = ``.split("\n");
+const testLines = `3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32`.split("\n");
 
 describe("Day 05", () => {
+  describe("parseDb", () => {
+    it("parses freshRanges", () => {
+      const db = parseDb(testLines);
+
+      expect(db.freshRanges.length).toBe(4);
+      expect(db.freshRanges[0]).toEqual({ start: 3, end: 5 });
+      expect(db.freshRanges[3]).toEqual({ start: 12, end: 18 });
+    });
+
+    it("parses ingredients", () => {
+      const db = parseDb(testLines);
+
+      expect(db.ingredients.length).toBe(6);
+      expect(db.ingredients[0]).toBe(1);
+      expect(db.ingredients[5]).toBe(32);
+    });
+  });
   describe("Part 1", () => {
     it("works", () => {
-      expect(part1(testLines)).toBe(0);
+      expect(part1(testLines)).toBe(3);
     });
   });
   describe("Part 2", () => {
--- src/05/index.ts
+++ src/05/index.ts
@@ -1,8 +1,35 @@
 import { readLines } from "../../util/readLines";
 
+type Range = { start: number; end: number };
+type Db = {
+  freshRanges: Range[];
+  ingredients: number[];
+};
+
+export function parseDb(lines: string[]): Db {
+  let blankLineIdx = lines.indexOf("");
+
+  const freshRanges = lines.slice(0, blankLineIdx).map((l) => {
+    const [start, end] = l.split("-").map((s) => parseInt(s, 10));
+    return { start, end };
+  });
+
+  const ingredients = lines.slice(blankLineIdx + 1).map((s) => parseInt(s, 10));
+
+  return {
+    freshRanges,
+    ingredients,
+  };
+}
+
+export function isInRange(x: number, ranges: Range[]) {
+  return ranges.some((r) => x >= r.start && x <= r.end);
+}
 // Part 1
 export function part1(lines: string[]) {
-  return 0;
+  const { freshRanges, ingredients } = parseDb(lines);
+  return ingredients.filter((ingredient) => isInRange(ingredient, freshRanges))
+    .length;
 }
 
 // Part 2
