0cd7ef60856e0c2c0928eda04125ae734137db37
Thomas Beirne <thomas@ediphi.com>
2025-12-12 22:23:19 -0800

    Day 6, part 2

--- src/06/index.test.ts
+++ src/06/index.test.ts
@@ -1,5 +1,13 @@
 import { describe, it, expect } from "vitest";
-import { calc, parseProblems, part1, part2 } from "./index";
+import {
+  calc,
+  parseProblemsV1,
+  parseProblemsV2,
+  rotateLeft,
+  part1,
+  part2,
+  splitBySentinel,
+} from "./index";
 
 const testLines = `123 328  51 64 
  45 64  387 23 
@@ -7,19 +15,19 @@ const testLines = `123 328  51 64
 *   +   *   +  `.split("\n");
 
 describe("Day 06", () => {
-  describe("parseProblems", () => {
+  describe("calc", () => {
     it("works", () => {
-      const problems = parseProblems(testLines);
-
-      expect(problems.length).toBe(4);
-      expect(problems[0]).toEqual({ operands: [123, 45, 6], operator: "*" });
+      const problems = parseProblemsV1(testLines);
+      expect(calc(problems[0])).toBe(33210);
     });
   });
 
-  describe("calc", () => {
+  describe("parseProblemsV1", () => {
     it("works", () => {
-      const problems = parseProblems(testLines);
-      expect(calc(problems[0])).toBe(33210);
+      const problems = parseProblemsV1(testLines);
+
+      expect(problems.length).toBe(4);
+      expect(problems[0]).toEqual({ operands: [123, 45, 6], operator: "*" });
     });
   });
 
@@ -29,9 +37,46 @@ describe("Day 06", () => {
     });
   });
 
+  describe("rotateLeft", () => {
+    it("works", () => {
+      expect(
+        rotateLeft([
+          [1, 2, 3],
+          [4, 5, 6],
+        ])
+      ).toEqual([
+        [3, 6],
+        [2, 5],
+        [1, 4],
+      ]);
+    });
+  });
+
+  describe("splitBySentinel", () => {
+    it("works", () => {
+      const result = splitBySentinel(
+        ["1", "23", "", "4", "", "56", "1200", "34"],
+        ""
+      );
+      expect(result.length).toBe(3);
+      expect(result[0]).toEqual(["1", "23"]);
+      expect(result[1]).toEqual(["4"]);
+      expect(result[2]).toEqual(["56", "1200", "34"]);
+    });
+  });
+
+  describe("parseProblemsV2", () => {
+    it("works", () => {
+      const problems = parseProblemsV2(testLines);
+
+      expect(problems.length).toBe(4);
+      expect(problems[0]).toEqual({ operands: [356, 24, 1], operator: "*" });
+    });
+  });
+
   describe("Part 2", () => {
     it("works", () => {
-      expect(part2(testLines)).toBe(0);
+      expect(part2(testLines)).toBe(3263827);
     });
   });
 });
--- src/06/index.ts
+++ src/06/index.ts
@@ -1,3 +1,4 @@
+import { createRange } from "util/range";
 import { readLines } from "../../util/readLines";
 
 type Operator = "+" | "*";
@@ -5,7 +6,14 @@ type Problem = { operands: number[]; operator: Operator };
 
 const parseItems = (line: string) => line.split(" ").filter((x) => x);
 
-export function parseProblems(lines: string[]): Problem[] {
+export function calc({ operands, operator }: Problem): number {
+  return operator === "+"
+    ? operands.reduce((a, b) => a + b)
+    : operands.reduce((a, b) => a * b);
+}
+
+// Part 1
+export function parseProblemsV1(lines: string[]): Problem[] {
   const operandRows = lines
     .slice(0, -1)
     .map((l) => parseItems(l).map((s) => parseInt(s, 10)));
@@ -19,22 +27,61 @@ export function parseProblems(lines: string[]): Problem[] {
     operator: operatorRow[i],
   }));
 }
-
-export function calc({ operands, operator }: Problem): number {
-  return operator === "+"
-    ? operands.reduce((a, b) => a + b)
-    : operands.reduce((a, b) => a * b);
-}
-
-// Part 1
 export function part1(lines: string[]) {
-  const problems = parseProblems(lines);
+  const problems = parseProblemsV1(lines);
   return problems.map(calc).reduce((a, b) => a + b);
 }
 
+export function rotateLeft<T>(matrix: T[][]): T[][] {
+  const width = matrix[0].length; // 3
+  const height = matrix.length; // 2
+
+  return createRange(0, width - 1).map((row) =>
+    createRange(0, height - 1).map((col) => matrix[col][width - row - 1])
+  );
+}
+
+export function splitBySentinel<T>(arr: T[], sentinel: T): T[][] {
+  return arr
+    .reduce(
+      (acc, x): T[][] => {
+        if (x === sentinel) {
+          return [...acc, []];
+        } else {
+          return [...acc.slice(0, -1), [...acc[acc.length - 1], x]];
+        }
+      },
+      [[]]
+    )
+    .filter((x) => x.length > 0);
+}
+
 // Part 2
+export function parseProblemsV2(lines: string[]): Problem[] {
+  const operandLines = lines.slice(0, -1);
+  const operandMatrix = operandLines.map((l) => l.split(""));
+  const rotatedMatrix = rotateLeft(operandMatrix);
+  const operands = splitBySentinel(
+    rotatedMatrix.map((l) => l.join("").trim()),
+    ""
+  )
+    .map((opStrList) => opStrList.map((s) => parseInt(s, 10)))
+    .reverse();
+
+  const operatorRow = parseItems(lines[lines.length - 1]).map(
+    (operator) => operator as Operator
+  );
+  const numProblems = operatorRow.length;
+
+  return new Array(numProblems).fill(null).map((_, i) => ({
+    operands: operands[i],
+    operator: operatorRow[i],
+  }));
+}
+
 export function part2(lines: string[]) {
-  return 0;
+  const problems = parseProblemsV2(lines);
+  return problems.map(calc).reduce((a, b) => a + b);
 }
 
 export function run() {
