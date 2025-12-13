e940d016261878f3e19d3d52f46adb2fbf20b0f0
Thomas Beirne <thomas@ediphi.com>
2025-12-12 21:30:50 -0800

    Day 6

--- src/06/index.test.ts
+++ src/06/index.test.ts
@@ -1,14 +1,34 @@
 import { describe, it, expect } from "vitest";
-import { part1, part2 } from "./index";
+import { calc, parseProblems, part1, part2 } from "./index";
 
-const testLines = ``.split("\n");
+const testLines = `123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  `.split("\n");
 
 describe("Day 06", () => {
-  describe("Part 1", () => {
+  describe("parseProblems", () => {
     it("works", () => {
-      expect(part1(testLines)).toBe(0);
+      const problems = parseProblems(testLines);
+
+      expect(problems.length).toBe(4);
+      expect(problems[0]).toEqual({ operands: [123, 45, 6], operator: "*" });
     });
   });
+
+  describe("calc", () => {
+    it("works", () => {
+      const problems = parseProblems(testLines);
+      expect(calc(problems[0])).toBe(33210);
+    });
+  });
+
+  describe("Part 1", () => {
+    it("works", () => {
+      expect(part1(testLines)).toBe(4277556);
+    });
+  });
+
   describe("Part 2", () => {
     it("works", () => {
       expect(part2(testLines)).toBe(0);
--- src/06/index.ts
+++ src/06/index.ts
@@ -1,8 +1,35 @@
 import { readLines } from "../../util/readLines";
 
+type Operator = "+" | "*";
+type Problem = { operands: number[]; operator: Operator };
+
+const parseItems = (line: string) => line.split(" ").filter((x) => x);
+
+export function parseProblems(lines: string[]): Problem[] {
+  const operandRows = lines
+    .slice(0, -1)
+    .map((l) => parseItems(l).map((s) => parseInt(s, 10)));
+
+  const operatorRow = parseItems(lines[lines.length - 1]).map(
+    (operator) => operator as Operator
+  );
+  const numProblems = operandRows[0].length;
+  return new Array(numProblems).fill(null).map((_, i) => ({
+    operands: operandRows.map((r) => r[i]),
+    operator: operatorRow[i],
+  }));
+}
+
+export function calc({ operands, operator }: Problem): number {
+  return operator === "+"
+    ? operands.reduce((a, b) => a + b)
+    : operands.reduce((a, b) => a * b);
+}
+
 // Part 1
 export function part1(lines: string[]) {
-  return 0;
+  const problems = parseProblems(lines);
+  return problems.map(calc).reduce((a, b) => a + b);
 }
 
 // Part 2
