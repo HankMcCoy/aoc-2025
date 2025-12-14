62dc1acc4be0e1de8949e9d89cca3e342703aab0
Thomas Beirne <thomas@ediphi.com>
2025-12-13 16:31:41 -0800

    Day 9, part 1

--- src/09/index.test.ts
+++ src/09/index.test.ts
@@ -1,12 +1,19 @@
 import { describe, it, expect } from "vitest";
 import { part1, part2 } from "./index";
 
-const testLines = ``.split("\n");
+const testLines = `7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3`.split("\n");
 
 describe("Day 09", () => {
   describe("Part 1", () => {
     it("works", () => {
-      expect(part1(testLines)).toBe(0);
+      expect(part1(testLines)).toBe(50);
     });
   });
   describe("Part 2", () => {
--- src/09/index.ts
+++ src/09/index.ts
@@ -1,8 +1,37 @@
+import { createRange } from "util/range";
 import { readLines } from "../../util/readLines";
+
+type Coord = { x: number; y: number };
+type CoordStr = `${number},${number}`;
+
+function parseCoord(coordStr: string): Coord {
+  const [x, y] = coordStr.split(",").map((s) => parseInt(s, 10));
+  return { x, y };
+}
+
+function parseLines(lines: string[]) {
+  return lines.map(parseCoord);
+}
+
+const calcArea = (c1: Coord, c2: Coord) =>
+  (Math.abs(c1.x - c2.x) + 1) * (Math.abs(c1.y - c2.y) + 1);
+
+function calcAreas(coords: Coord[]): { c1: Coord; c2: Coord; area: number }[] {
+  return createRange(0, coords.length - 2).flatMap((i) =>
+    createRange(i + 1, coords.length - 1).map((j) => {
+      const [c1, c2] = [coords[i], coords[j]];
+      return { c1, c2, area: calcArea(c1, c2) };
+    })
+  );
+}
 
 // Part 1
 export function part1(lines: string[]) {
-  return 0;
+  const coords = parseLines(lines);
+  const areas = calcAreas(coords);
+
+  const { area } = areas.sort((a, b) => b.area - a.area)[0];
+  return area;
 }
 
 // Part 2
